885d4511398393a6337fd9203bb7de30417b7f08
AL-Buscador <donpost@internet.ru>
2022-02-27 10:32:15 +0300

    first commit init

---
+++ src/Home/Main.java
@@ -0,0 +1,24 @@
+package Home;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        StreamMain.main();
+        System.out.println();
+	    List<Integer> intlist = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> filterList = new ArrayList<>();
+
+        for(int item : intlist) {
+            if (item > 0 && item % 2 == 0) {
+                filterList.add(item);
+            }
+        }
+
+        Collections.sort(filterList);
+        for (int item : filterList) {
+            System.out.println(item);
+        }
+    }
+}
